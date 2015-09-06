fe9c6689869bf72a4e6528582ac1c3a042fc9424
T Jay Carter <letter.t.carter@gmail.com>
2015-09-06 11:04:00 -0600

    assignment 2
    worked on the html and javascript for assignment 2

---
+++ week2/assignment/js/main.js
@@ -0,0 +1,42 @@
+var alabama;
+var alaska;
+var arizona;
+var arkansas;
+var california;
+var colorado;
+
+alabama = document.getElementById('alabama');
+	var ourFunction = function(){
+	    alert('The state capital of Alabama is Montgomery');
+	}
+	alabama.addEventListener('mouseenter', ourFunction);
+
+alaska = document.getElementById('alaska');
+	var ourFunction = function(){
+	    alert('The state capital of Alaska is Juneau');
+	}
+	alaska.addEventListener('mouseenter', ourFunction);
+
+arizona = document.getElementById('arizona');
+	var ourFunction = function(){
+	    alert('The state capital of Arizona is Phoenix');
+	}
+	arizona.addEventListener('mouseenter', ourFunction);
+
+arkansas = document.getElementById('arkansas');
+	var ourFunction = function(){
+		alert('The state capital of Arkansas is Little Rock')
+	}
+	arkansas.addEventListener('mouseenter', ourFunction);
+
+california = document.getElementById('california');
+	var ourFunction = function(){
+		alert('The state capital of California is Sacramento');
+	}
+	california.addEventListener('mouseenter', ourFunction);
+
+colorado = document.getElementById('colorado');
+	var ourFunction = function(){
+		alert('The state capital of Colorado is Denver');
+	}
+	colorado.addEventListener('mouseenter', ourFunction);
