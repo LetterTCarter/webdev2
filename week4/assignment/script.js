909ef3cc9d523074ddf11591ef26e0eff672ffa3
T Jay Carter <letter.t.carter@gmail.com>
2015-09-19 12:05:29 -0600

    assignment 4 js
    wrote the JS for assignment 4

--- week4/assignment/script.js
+++ week4/assignment/script.js
@@ -1,15 +1,39 @@
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Donate" button.
+  console.log("There are " + speechArray.length + " speeches on the page.");
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Churchill" button.
+  console.log("This speech was written by " + theirFinestHour.author + " in " + theirFinestHour.year + ". It is " + theirFinestHour.bce + " that this year is B.C.E.");
 });
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Ghandi" button.
+  console.log("This speech was written by " + quitIndia.author + " in " + quitIndia.year + ". It is " + quitIndia.bce + " that this year is B.C.E.");
 });
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Demosthenes" button.
+  console.log("This speech was written by " + theThirdPilippic.author + " in " + theThirdPilippic.year + ". It is " + theThirdPilippic.bce + " that this year is B.C.E.");
 });
+
+var theirFinestHour = {
+	'author' : "Winston Churchill",
+	'year' : 1940,
+	'bce' : false
+};
+
+var quitIndia = {
+	'author' : "Ghandi",
+	'year' : 1942,
+	'bce' : false
+};
+
+var theThirdPilippic = {
+	'author' : "Demosthenes",
+	'year' : 342,
+	'bce' : true
+};
+
+var speechArray = [theirFinestHour, quitIndia, theThirdPilippic];
+
+var yearBetweenSpeeches = quitIndia.year - theirFinestHour.year;
+
+console.log(quitIndia.author + "'s speech and " + theirFinestHour.author + "'s speech are " + yearBetweenSpeeches + " years apart.");
