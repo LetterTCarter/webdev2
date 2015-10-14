d946e2367aa9b655b39b8ab8ff7cc8ca0ba8ee8b
T Jay Carter <letter.t.carter@gmail.com>
2015-10-13 20:48:12 -0600

    working on exercise 8.1
    playing with a repeatable function

--- week8/script.js
+++ week8/script.js
@@ -1,12 +1,18 @@
+var divs = getElementsByTagName('div');
+
 function resizeBox() {
-document.getElementById("exercise").onmouseover = function() {
-	if (document.getElementById("exercise").className == "clickStyle") {
-		document.getElementById("exercise").className = "";
+document.getElementById('exercise').onclick = function() {
+	if (document.getElementById('exercise').className == "clickStyle") {
+		document.getElementById('exercise').className = "";
 	} else {
-		document.getElementById("exercise").className = "clickStyle";
+		document.getElementById('exercise').className = "clickStyle";
 	};
 }};
 
-window.onload = function() {
-	resizeBox();
-}
+document.getElementById('exercise').addEventListener('click', function(){
+  resizeBox();
+});
+
+document.getElementById('exercise2').addEventListener('click', function(){
+  resizeBox();
+});
