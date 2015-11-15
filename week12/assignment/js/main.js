68d4de0f48d7f2e5955a341af7f0e6ea6d353dec
T Jay Carter <tjaycarter@T-Jay-Carters-Mac-Pro.local>
2015-11-14 21:02:17 -0700

    basic html and css done. jquery slow going.

--- week12/assignment/js/main.js
+++ week12/assignment/js/main.js
@@ -1,3 +1,33 @@
 $(function () {
-  console.log("Document ready is loaded.")
+  console.log("Document ready is loaded.");
+  //build the nav menu
+  //select all of the recipe headings
+  var menuItems = $('article h2');
+  //create an unordered list to hold the headings
+  var list = $("<ul id='bookmarks'>");
+  //create a counter to generate a unique tag name for each anchor
+  var i = 0;
+
+  //give each recipe heading a named anchor and a link for the list and set the HTML of the h2 to contain the link destination
+  menuItems.each(function(indx, elm){
+    $(this).html("<a name='bookmark" + i + "'></a>" + $(this).html());
+  //make a new LI tag for the list that links to the anchor tag and has the same text as the H2
+  list.append($("<li><a href='#bookmark" + i++ + "'> " + $(this).text() + "</a></li>").addClass('btn a'));
+});
+//insert the new table of contents list in the navDiv
+$('#navDiv').html(list);
+
+  //select all of the articles
+  var
+    recipeArticles = $('article'),
+    link = $(this).html("<a name='bookmark" + i + "'></a>" + $(this).html()),
+    j = 0;
+
+    //give each recipe article a unique ID
+    recipeArticles.each(function(indx, elm){
+      $(this).attr("id", "recipe" + j++);
+      $(link).click(function() {
+        $("#recipe0").fadeOut("slow");
+      });
+  });
 });
