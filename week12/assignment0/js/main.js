4703f4628f4635400361fd3d0e2124cade917fc7
T Jay Carter <tjaycarter@T-Jay-Carters-Mac-Pro.local>
2015-11-15 17:10:41 -0700

    brain fried on assignment 12.
    ugh. Need more study. I just don’t quite have it yet. But I’m getting
    closer.

--- week12/assignment0/js/main.js
+++ week12/assignment0/js/main.js
@@ -1,31 +1,47 @@
 $(function () {
   console.log("Document ready is loaded.");
-  //create the nav menu by grabbing h1 titles
+  //grab all of the article h2s for the nav
 	var $recipeItems = $('article h2');
-	//this function builds the nav and all of it's functionality
+	//build the nav and make it function properly
 	$recipeItems.each(function(i){
-		//create unique id for each article containing H1 recipe title
+		//assign a unique id to the h2s with the h2 text
 		$(this).parent().attr('id', 'recipe-' + i);
 		var navText = $(this).text();
 		//create an li and a element that links to cooresponding recipe title
 		$('#navDiv').append('<a href="#recipe-' + i + '"><li>' + navText + '</li></a>').addClass('btn');
 
-		var //vars created AFTER nav is built
+		var //created for menu functionality
 			$link = $('a[href="#recipe-' + i + '"]'),
-			$linkLi = $('a[href="#recipe-' + i + '"] li')
-		;
+			$linkLi = $('a[href="#recipe-' + i + '"] li');
 		//click any link
 		$link.click(function(){
 			//look for class current, fade it out...
 			$('.current').animate({opacity: 0}, 200, function(){
 				$(this).removeClass('current');//remove current class....
+        $('article h1').detach();
 			});
 			//find the recipe that cooresponds to link and fade it in
-			$('#recipe-' + i).animate({opacity: 1}, 1400, function(){
+			$('#recipe-' + i).animate({opacity: 1}, 2000, function(){
 				$(this).addClass('current');//add class current
-			})
+        $('article h2').prepend("<h1>What a tasty choice!</h1>");
+			});
 		});
+    //add hover class on link hover
+    $linkLi.hover(function() {
+      $(this).toggleClass('hover');
+    });
   });
-//insert the new menuItems list in the navDiv
-$('#navDiv').html(list);
+  $('#navDiv h3').hover(function() {
+    $(this).toggleClass('hover'),
+    $(this).click(function(){
+      $('#contentDiv').fadeOut(2000);
+    });
+  });
+  animateHeader();
 });
+function animateHeader() {
+	$("header").animate({
+    width: "100%",
+    opacity: 1
+  }, 2000, "swing");
+};
