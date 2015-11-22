d6845c16c0b930e133bfe6e02ca39d1b6b5b0a50
T Jay Carter <tjaycarter@T-Jay-Carters-Mac-Pro.local>
2015-11-21 19:55:58 -0700

    assignment 13
    reinvestigated assignment 2

---
+++ week13/assignment/js/main.js
@@ -0,0 +1,26 @@
+$(function() {
+	console.log("Document is ready for action");
+
+	//hide the initial quiz area
+	$('#mainContent article').hide();
+	$('.state p').hide();
+
+	//function to display states on menu item click
+	$('li').click(function() {
+		var selectedArticle = $("#" + this.id + "Quiz");
+        if ($(selectedArticle).is(":visible")) {
+          $(selectedArticle).fadeOut({duration: 800});
+        } else {
+          $("#mainContent article").hide(),
+					$('.state p').hide();
+          $(selectedArticle).slideDown(1000, "swing");
+        };
+				$("#home h4").replaceWith("<h4>Click the state for the answer!</h4>");
+				$('article img').click(function() {
+					$('.state p').show().removeClass("quiz").addClass("visible");
+				})
+		});
+	$('header h1').click(function() {
+		location.reload();
+	});
+});
